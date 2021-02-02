8a63df842919bd7899da05791badf5caf1be4892
DKlaver <dklaver15@gmail.com>
2021-02-02 14:19:45 -0600

    Added a method to create the initial 20x20 game board.

--- game.js
+++ game.js
@@ -29,6 +29,8 @@ class Game {
 
 		this.displayRules();
 
+		console.log(this.generateGameBoard());
+
 	}
 
 	playerAdd() {
@@ -44,13 +46,28 @@ class Game {
 		console.log("Please read the following rules carefuly:")
 		console.log(this.playerOne.name + " will be given a chance to position his ships on his or her board first. Please keep in mind that ships may no overlap or extend outside the boundry of the board.");
 		console.log(this.playerTwo.name + " will then also be given a chance to position his or her ships on his board. Please keep in mind that ships may no overlap or extend outside the boundry of the board.");
-		console.log("Once both players have had a chance to position their ships, " + this.playerOne.name + " will then have the opportunity to choose a column followed by a row for the location of his or her first shot.");
+		console.log("Once both players have had a chance to position their ships, " + this.playerOne.name + " will then have the opportunity to choose a row followed by a column for the location of his or her first shot.");
 		console.log(this.playerTwo.name + " will then have the same opportunity.");
 		console.log("The game will continue as such until one of the players has sunk all of his or her opponent's ships.");
 		console.log("The first player to sink all of his or her opponent's ships is the winner.");
 	}
 
+	generateGameBoard() {
+		let gameBoard = [];
+		let rows = 20;
+		let rowID = "";
+		let columns = 20;
+		let columnID = 1;
 
+		for (let c = 1; c <= columns; c++) {
+			let beginningChar = 65;
+			for (let r = 0; r < rows; r++) {
+				gameBoard.push([String.fromCharCode(beginningChar), c]);
+				beginningChar++;
+			}
+		}
+		return gameBoard;
+	}
 
 
 
