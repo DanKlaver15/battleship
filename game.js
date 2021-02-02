5172e4b4d742aec6ed75254bdd7aa8d172d64cf1
DKlaver <dklaver15@gmail.com>
2021-02-02 12:52:17 -0600

    Created an array of the ships needed for the game in the game class constructor.

--- game.js
+++ game.js
@@ -9,3 +9,29 @@ const battleship = require("./battleship.js");
 const Battleship = battleship.Battleship;
 const carrier = require("./carrier.js");
 const Carrier = carrier.carrier;
+
+/*====================================================================*/
+
+class Game {
+	constructor() {
+		this.ships = [];
+
+		this.ships.push(new Destroyer());
+		this.ships.push(new Submarine());
+		this.ships.push(new Battleship());
+		this.ships.push(new Carrier());
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
