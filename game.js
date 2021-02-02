27e8ab3081ea95a1e49106e45b10bfd8cba1a5b7
DKlaver <dklaver15@gmail.com>
2021-02-02 12:47:10 -0600

    Created initial statements required for import of the various classes.

--- game.js
+++ game.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const prompt = require("prompt-sync")();
+const destroyer = require("./destroyer.js");
+const Destroyer = destroyer.Destroyer;
+const submarine = require("./submarine.js");
+const Submarine = submarine.Submarine;
+const battleship = require("./battleship.js");
+const Battleship = battleship.Battleship;
+const carrier = require("./carrier.js");
+const Carrier = carrier.carrier;
